4d4394f75664594b869b94256f9b86239ef1ca3a
IgorLorentz <igorlorentz81@gmail.com>
2024-06-09 12:42:47 -0300

    Create RodCutting

--- src/RodCutting.java
+++ src/RodCutting.java
@@ -1,8 +1,18 @@
 public class RodCutting {
-    public int RodCutting(){
+    public int RodCutting(int preço[], int index, int n) {
+        if (index == 0)
+            return n * preço[0];
+        if (n == 0)
+            return 0;
 
+        int notCut = RodCutting(preço, index - 1, n);
+        int cut = Integer.MIN_VALUE;
+        int rod_length = index + 1;
+        int new_length = n - rod_length;
 
+        if (rod_length <= n)
+            cut = preço[index] + RodCutting(preço, new_length - 1, new_length);
 
-
+        return Math.max(notCut, cut);
     }
 }
