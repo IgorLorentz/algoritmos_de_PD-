0240ae2ad8d9b9d42509d288da58266a7549e810
IgorLorentz <igorlorentz81@gmail.com>
2024-06-09 12:43:35 -0300

    Edit FloydWarshall

--- src/FloydWarshall.java
+++ src/FloydWarshall.java
@@ -1,16 +1,14 @@
 public class FloydWarshall {
-    public int FloydWarshall(){
-        for(int k=0; ){
-            for(){
-                for(){
-                    if(){
-
+    public void FloydWarshall(int caminho[][], int v){
+        for(int k = 0; k < v; k++){
+            for(int i = 0; i < v; i++){
+                for(int j = 0; j < v; j++){
+                    if(caminho[i][j] > caminho[i][k] + caminho [j][k]){
+                        caminho[i][j] =  caminho[i][k] + caminho[j][k];
                     }
                 }
             }
 
         }
-
-        return 1;
     }
 }
